cfdd1db25e0aeab0a2ac329804a0aecdb8f9667c
Shanshan Hou <shanshan.hou@aalto.fi>
2021-05-26 21:09:16 +0300

    1.7 is finished, need to rewrite

--- part1/src/App.js
+++ part1/src/App.js
@@ -18,9 +18,11 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all,setAll]=useState(0)
+ // const [positive, setPositive]=useState(0)
 
   const setToValue=(setValue, newValue)=>()=>{setValue(newValue+1)}
-
+  
 
   return (
     <div>
@@ -33,6 +35,11 @@ const App = () => {
       <Display value = {good} text = 'good' />
       <Display value = {neutral} text = 'neutral' />
       <Display value = {bad} text = 'bad' />
+      <Display value ={bad+good+neutral} text='all' />
+      <Display value = {(bad+good+neutral)/3} text='average'/>
+      <p>positive : {good/(bad+good+neutral)*100} %</p>
+      
+
 
     </div>
   )
