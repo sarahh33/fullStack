82c8845180172e240dd7eab23645a81c5a4f6a1a
Shanshan Hou <shanshan.hou@aalto.fi>
2021-06-01 11:37:41 +0300

    2.1 finished

---
+++ part2/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react'
+const Header= ({name})=>(<h1>{name}</h1>)
+
+
+const Content =({name,number})=>{
+    return(
+    <p>{name} {number}</p>
+    )
+}
+
+const Course = ({course})=>{
+    
+    
+    return (
+        <div>
+            <Header name = {course.name} />
+            <ul>
+                {course.parts.map(part => 
+                <Content key ={part.id} name = {part.name} number = {part.exercises}/>
+                ) }
+            </ul>
+            
+            </div>
+    )
+}
+
+
+const App = () => {
+    const course = {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return <Course course={course} />
+  }
+
+export default App
