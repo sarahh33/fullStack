cca86a580b73634e85cbaaff8442d6e286977478
Shanshan Hou <shanshan.hou@aalto.fi>
2021-07-22 12:23:15 +0300

    4.9 finished

--- part4/blogList/tests/note_api.test.js
+++ part4/blogList/tests/note_api.test.js
@@ -4,17 +4,19 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-  })
 
 test('the blog list application returns the correct amount of blog posts', async () =>{
     const response = await api.get('/api/blogs')
 
   expect(response.body).toHaveLength(3)
+})
+
+test('the unique property _id exists', async () => {
+    const response = await api.get('/api/blogs')
+    const unique_id = response.body.map(r=>r._id)
+    console.log(unique_id)
+
+    expect(unique_id).toBeDefined()
 })
 
 afterAll(() => {
