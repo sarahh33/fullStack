4ec1330f0a05e47a2aed45def75020c49fa1c70f
Shanshan Hou <shanshan.hou@aalto.fi>
2021-08-19 11:31:45 +0300

    6.11 finished, timer works

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -3,7 +3,7 @@ const initialState = 'Nothing happens yet'
 const notiReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'VOTING_NOTI':
-      return `You votes '${action.data.content}' `
+      return `You voted '${action.data.content}' `
     case 'INITIAL':
       return initialState
     default:
